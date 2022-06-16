b4b931dfd59980c11546b2a02cd6adcf8eed27dd
Xocash695 <akash.kallumkal@gmail.com>
2022-06-15 20:18:18 -0400

    added container

--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,7 @@
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { Container } from "react-bootstrap";
+import "./Login.css";
 function Login() {
     return (
         <>
@@ -8,15 +9,15 @@ function Login() {
                 <Form>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="name@example.com" required/>
+                        <Form.Control type="email" placeholder="name@example.com" required />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" required/>
+                        <Form.Control type="password" placeholder="Password" required />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="I accept the terms and conditions" required/>
+                        <Form.Check type="checkbox" label="I accept the terms and conditions" required />
                     </Form.Group>
                     <Button variant="primary" type="submit">
                         Submit
