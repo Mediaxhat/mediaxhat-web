723d26d927c8d9f66997d736d1043559dfad0e02
Xocash695 <akash.kallumkal@gmail.com>
2022-06-15 20:29:04 -0400

    rm login.css

--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,6 @@
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { Container } from "react-bootstrap";
-import "./Login.css";
 function Login() {
     return (
         <>
