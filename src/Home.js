e6c20a405c9ae31cb928d458af6b9fd5b730bf64
Xocash695 <akash.kallumkal@gmail.com>
2022-07-11 17:08:47 -0400

    added absec backround & Navbar name

--- src/Home.js
+++ src/Home.js
@@ -9,21 +9,24 @@ function Home() {
                 <div id="intro">
                     <div className="transbox">
                         <h1 className="title">A Re-defined Chat focused on privacy<span id="icon"></span></h1>
-                        <i className="bi bi-lock-fill"></i>
+                        <span className="icon"><i className="bi bi-lock-fill"></i></span>
                         <br></br>
                         <Button variant="primary">Try the Chat</Button>
                     </div>
                 </div>
                 <div id="opensc">
                     <div className="transbox">
-                        <h1 className="title">Open source<br></br><span id="icon"><i className="bi bi-file-earmark-code-fill"></i></span></h1>
+                        <h1 className="title">Open source<br></br><span className="icon"><i className="bi bi-file-earmark-code-fill"></i></span></h1>
                         <Button variant="primary">View code</Button>
                     </div>
                 </div>
                 <div id="about">
-                    <h1 className="title">About</h1>
-                    <p> Mediaxhat is devloped by Akash k</p>
-                    <p> For inquires please email: akash.kallumkal@gmail.com </p>
+                    <div className="transbox">
+                        <h1 className="title">About:</h1>
+                        <br></br>
+                        <p> Mediaxhat is devloped by Akash k</p>
+                        <p> For inquires please email: akash.kallumkal@gmail.com </p>
+                    </div>
                 </div>
             </NoInternet>
         </>
--- src/NavBar.js
+++ src/NavBar.js
@@ -2,18 +2,20 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import logo from "./M.png";
+import "./NavBar.css"
 function NavBar() {
   return (
     <><Navbar bg="light" variant="light" expand="lg" sticky="top">
       <Container>
         <Navbar.Brand href="/">
           <img src={logo} width="30" height="30" alt="Logo"></img>
+          <span id="Mediaxhat">Mediaxhat</span>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/#absec">About</Nav.Link>
+            <Nav.Link href="/#about">About</Nav.Link>
           </Nav>
           <Nav className="ms-auto">
             <Nav.Link href="/signup">Signup</Nav.Link>
